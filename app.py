0c3bf51853b732223d91f88ba0584d67509875b9
Andre Marotta Pegolaro <andre.marotta@mercadopago.com.br>
2026-02-25 18:59:30 -0300

    Initial commit: Fury service setup

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/ping')
+def ping():
+    return 'pong', 200
+
+@app.route('/')
+def home():
+    return 'Meu servico Fury esta funcionando!', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
